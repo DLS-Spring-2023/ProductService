63776a64476894c1f89442f86d4bd4b4eab894f3
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2023-05-12 15:10:24 +0200

    added jwt auth

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use(authenticateJWT)
+app.use(authenticateJWT)
 
 app.use(productsRouter)
 
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('??');
 })
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}🍔`);
 });
--- src/auth/verify-token-middleware.ts
+++ src/auth/verify-token-middleware.ts
@@ -1,19 +1,23 @@
 import jwt from 'jsonwebtoken';
-
-export const authenticateJWT = (req, res, next) => {
+export const authenticateJWT = async (req, res, next) => {
   const authHeader = req.headers.authorization;
+
+  const publicKey = await fetch("https://dls-auth.azurewebsites.net/v1/keys/project/f4d470e24411001")
+  .then(res => res.json())
+  .then(json => json.data.publicKey)
 
   if (authHeader) {
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, process.env.PUBLIC_KEY, (err, user) => {
+    jwt.verify(token, publicKey, (err) => {
       if (err) {
+        console.log(err);
         return res.sendStatus(403);
       }
 
-      req.user = user;
       next();
     });
+
   } else {
     res.sendStatus(401);
   }
