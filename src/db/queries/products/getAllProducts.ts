5c8429d037f88f3943c2afb0ce89a37d35b067f6
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2023-05-20 22:07:16 +0200

    fixed getting only non-tombstone records

--- src/db/queries/products/getAllProducts.ts
+++ src/db/queries/products/getAllProducts.ts
@@ -1,13 +1,17 @@
 import { query } from '../../connection/database';
-
 import { IProduct } from '../../../entities/products/product';
 
 export const getAllProducts = async (): Promise<IProduct[]> => {
   try {
-    const result = await query<IProduct[]>('SELECT * FROM product_description');
+    const result = await query<IProduct[]>(`
+      SELECT *
+      FROM product_description
+      WHERE product_id NOT IN (SELECT product_id FROM product_removed)
+    `);
 
     return result;
   } catch (error) {
     console.log(error);
+    throw error;
   }
 };
